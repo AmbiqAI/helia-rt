b8245251a540978487bc65cd1b8166edc249edc9
Adam Page <apage224@gmail.com>
2024-12-04 10:11:04 -0600

    feat: LeakyReLU update.

--- tensorflow/lite/micro/kernels/ambiq/leaky_relu.cc
+++ tensorflow/lite/micro/kernels/ambiq/leaky_relu.cc
@@ -36,35 +36,56 @@ void leaky_relu_s8(
     int8_t* output_data
   ) {
 
-  const in32_t input_offset = data.input_zero_point;
-  const in32_t output_offset = data.output_zero_point;
-  const in32_t output_multiplier_alpha = data.output_multiplier_alpha;
-  const in32_t output_shift_alpha = data.output_shift_alpha;
-  const in32_t output_multiplier_identity = data.output_multiplier_identity;
-  const in32_t output_shift_identity = data.output_shift_identity;
+  const int32_t input_offset = data.input_zero_point;
+  const int32_t output_offset = data.output_zero_point;
+  const int32_t output_multiplier_alpha = data.output_multiplier_alpha;
+  const int32_t output_shift_alpha = data.output_shift_alpha;
+  const int32_t output_multiplier_identity = data.output_multiplier_identity;
+  const int32_t output_shift_identity = data.output_shift_identity;
 
-  int flat_size = MatchingFlatSize(input_shape, output_shape);
-  static const int32_t quantized_min = std::numeric_limits<int8_t>::min();
-  static const int32_t quantized_max = std::numeric_limits<int8_t>::max();
+  int32_t flat_size = MatchingFlatSize(input_shape, output_shape);
+  const int32_t quantized_min = std::numeric_limits<int8_t>::min();
+  const int32_t quantized_max = std::numeric_limits<int8_t>::max();
 
 #if defined(ARM_MATH_MVEI)
   // Perform 4 operations in parallel
   uint32_t blkCnt = (flat_size + 3) / 4;
-  int32x4_t res;
+
+  #ifdef CMSIS_NN_USE_SINGLE_ROUNDING
+    const int32_t right_shift_alpha = MIN(-1, output_shift_alpha);
+    const int32_t left_shift_alpha = output_shift_alpha - right_shift_alpha;
+    const int32_t right_shift_identity = MIN(-1, output_shift_identity);
+    const int32_t left_shift_identity = output_shift_identity - right_shift_identity;
+  #else
+    const int32_t left_shift_alpha = LEFT_SHIFT(output_shift_alpha);
+    const int32_t right_shift_alpha = -RIGHT_SHIFT(output_shift_alpha);
+    const int32_t left_shift_identity = LEFT_SHIFT(output_shift_identity);
+    const int32_t right_shift_identity = -RIGHT_SHIFT(output_shift_identity);
+  #endif
 
   while (blkCnt > 0U) {
     mve_pred16_t val_pred = vctp32q((uint32_t)flat_size);
-    // Load values in 32bit registers
-    res = vldrbq_z_s32(input_data, val_pred);
+    // Load values in 32-bit registers
+    int32x4_t res = vldrbq_z_s32(input_data, val_pred);
     // Subtract the input offset
     res = vsubq_s32(res, vdupq_n_s32(input_offset));
-    // For values < 0, apply the leaky relu
-    mve_pred16_t neg_pred = vcmpltq_n_s32(res, 0);
-    res = arm_requantize_mve(res, output_multiplier_alpha, output_shift_alpha);
-    res = arm_requantize_mve_pred(res, output_multiplier_alpha, output_shift_alpha, neg_pred);
-    // For values >= 0, apply the identity
-    mve_pred16_t pos_pred = vcmpgeq_n_s32(res, 0);
-    res = arm_requantize_mve_pred(res, output_multiplier_identity, output_shift_identity, pos_pred);
+    // For values < 0, apply alpha otherwise apply identity
+    mve_pred16_t alpha_pred = vcmpltq_n_s32(res, 0);
+    int32x4_t left_shift_ident_dup = vdupq_n_s32(left_shift_identity);
+    int32x4_t left_shift_dup = vdupq_m_n_s32(left_shift_ident_dup, left_shift_alpha, alpha_pred);
+    int32x4_t right_shift_ident_dup = vdupq_n_s32(right_shift_identity);
+    int32x4_t right_shift_dup = vdupq_m_n_s32(right_shift_ident_dup, right_shift_alpha, alpha_pred);
+    int32x4_t mult_ident_dup = vdupq_n_s32(output_multiplier_identity);
+    int32x4_t mult_dup = vdupq_m_n_s32(mult_ident_dup, output_multiplier_alpha, alpha_pred);
+  #ifdef CMSIS_NN_USE_SINGLE_ROUNDING
+    res = vqdmulhq_s32(vshlq_s32(res, left_shift_dup), mult_dup);
+    res = vrshlq_s32(res, right_shift_dup);
+  #else
+    res = vqrdmulhq_s32(vshlq_s32(res, left_shift_dup), mult_dup);
+    int32x4_t fixup = vshrq_n_s32(vandq_s32(res, right_shift_dup), 31);
+    int32x4_t fixed_up_dividend = vqaddq_s32(res, fixup);
+    res = vrshlq_s32(fixed_up_dividend, right_shift_dup);
+  #endif
     // Add the output offset
     res = vaddq_n_s32(res, output_offset);
     // Clamp the result
@@ -78,6 +99,32 @@ void leaky_relu_s8(
     blkCnt -= 1;
     flat_size -= 4;
   }
+
+  // while (blkCnt > 0U) {
+  //   mve_pred16_t val_pred = vctp32q((uint32_t)flat_size);
+  //   // Load values in 32bit registers
+  //   res = vldrbq_z_s32(input_data, val_pred);
+  //   // Subtract the input offset
+  //   res = vsubq_s32(res, vdupq_n_s32(input_offset));
+  //   // For values < 0, apply the leaky relu
+  //   mve_pred16_t neg_pred = vcmpltq_n_s32(res, 0);
+  //   res = arm_requantize_mve_pred(res, output_multiplier_alpha, output_shift_alpha, neg_pred);
+  //   // For values >= 0, apply the identity
+  //   mve_pred16_t pos_pred = vcmpgeq_n_s32(res, 0);
+  //   res = arm_requantize_mve_pred(res, output_multiplier_identity, output_shift_identity, pos_pred);
+  //   // Add the output offset
+  //   res = vaddq_n_s32(res, output_offset);
+  //   // Clamp the result
+  //   res = vmaxq_s32(res, vdupq_n_s32(quantized_min));
+  //   res = vminq_s32(res, vdupq_n_s32(quantized_max));
+  //   // Store the result
+  //   vstrbq_p_s32(output_data, res, val_pred);
+  //   // Increment pointers
+  //   input_data += 4;
+  //   output_data += 4;
+  //   blkCnt -= 1;
+  //   flat_size -= 4;
+  // }
 #else
 
   int32_t val;
@@ -94,6 +141,7 @@ void leaky_relu_s8(
   }
 
 #endif
+
 }
 
 
