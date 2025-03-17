cd10610815ede68c50c357d155030f6bb812dcbc
Evan Chen <88596241+evanchen11@users.noreply.github.com>
2025-03-17 14:34:09 -0700

    Evan/cumsum (#18)
    * adds int16 cumsum reference operation

--- tensorflow/lite/micro/kernels/cumsum.cc
+++ tensorflow/lite/micro/kernels/cumsum.cc
@@ -30,8 +30,8 @@ constexpr int kAxisTensor = 1;
 constexpr int kOutputTensor = 0;
 
 constexpr int kCumSumIntegerShift = 20;
-
-// only used with INT8 tensors
+constexpr int kCumSumIntegerShift_s16 = 10;
+// only used with INT8 and INT16 tensors
 struct OpData {
   int32_t output_activation_min;
   int32_t output_activation_max;
@@ -43,6 +43,88 @@ struct OpData {
   int output_shift;
   int left_shift;
 };
+
+
+inline void CumSum_s16(const ArithmeticParams& params, const int16_t* input_data,
+                   const RuntimeShape& shape, int32_t axis, bool exclusive,
+                   bool reverse, int16_t* output_data) {
+  TFLITE_DCHECK_LE(params.quantized_activation_min,
+                   params.quantized_activation_max);
+  TFLITE_DCHECK_GE(-params.input1_offset, std::numeric_limits<int16_t>::min());
+  TFLITE_DCHECK_LE(-params.input1_offset, std::numeric_limits<int16_t>::max());
+  const int32_t rank = shape.DimensionsCount();
+  TFLITE_DCHECK_GE(rank, 1);
+  TFLITE_DCHECK_GE(axis, 0);
+  TFLITE_DCHECK_LT(axis, rank);
+
+  size_t inner = 1;
+  size_t outer = 1;
+  size_t depth = 1;
+  for (int32_t i = 0; i < rank; i++) {
+    if (i < axis)
+      inner *= shape.Dims(i);
+    else if (i > axis)
+      outer *= shape.Dims(i);
+    else
+      depth = shape.Dims(i);
+  }
+
+  for (size_t outer_index = 0; outer_index < outer; outer_index++) {
+    size_t outer_index_adj;
+    if (reverse)
+      outer_index_adj = (outer - 1) - outer_index;
+    else
+      outer_index_adj = outer_index;
+    for (size_t inner_index = 0; inner_index < inner; inner_index++) {
+      int32_t accumulator = params.input1_offset;  // accumulator = 0
+      accumulator *= (1 << params.left_shift);
+      accumulator = MultiplyByQuantizedMultiplierSmallerThanOneExp(
+          accumulator, params.input1_multiplier, params.input1_shift);
+
+      size_t inner_index_adj;
+      if (reverse)
+        inner_index_adj = (inner - 1) - inner_index;
+      else
+        inner_index_adj = inner_index;
+
+      for (size_t depth_index = 0; depth_index < depth; depth_index++) {
+        size_t depth_index_adj;
+        if (reverse)
+          depth_index_adj = (depth - 1) - depth_index;
+        else
+          depth_index_adj = depth_index;
+
+        size_t index = outer_index_adj;
+        index += inner_index_adj * depth * outer;
+        index += depth_index_adj * outer;
+
+        const int32_t y = params.input1_offset + input_data[index];
+        const int32_t shifted_y = y * (1 << params.left_shift);
+        const int32_t scaled_y = MultiplyByQuantizedMultiplierSmallerThanOneExp(
+            shifted_y, params.input1_multiplier, params.input1_shift);
+
+        int32_t scaled_output;
+        if (exclusive) {
+          scaled_output = accumulator;
+          accumulator += scaled_y;
+        } else {
+          accumulator += scaled_y;
+          scaled_output = accumulator;
+        }
+
+        const int32_t raw_output =
+            MultiplyByQuantizedMultiplierSmallerThanOneExp(
+                scaled_output, params.output_multiplier, params.output_shift) +
+            params.output_offset;
+        const int32_t clamped_output =
+            std::min(params.quantized_activation_max,
+                     std::max(params.quantized_activation_min, raw_output));
+        output_data[index] = static_cast<int16_t>(clamped_output);
+      }
+    }
+  }
+}
+
 
 TfLiteStatus CalculateOpData(TfLiteContext* context, TfLiteNode* node) {
   TF_LITE_ENSURE_EQ(context, NumInputs(node), 2);
@@ -56,7 +138,7 @@ TfLiteStatus CalculateOpData(TfLiteContext* context, TfLiteNode* node) {
       micro_context->AllocateTempInputTensor(node, kAxisTensor);
 
   TF_LITE_ENSURE(context,
-                 input->type == kTfLiteFloat32 || input->type == kTfLiteInt8);
+                 input->type == kTfLiteFloat32 || input->type == kTfLiteInt8 || input->type == kTfLiteInt16);
   TF_LITE_ENSURE_EQ(context, axis->type, kTfLiteInt32);
 
   TF_LITE_ENSURE_EQ(context, NumElements(axis), 1);
@@ -95,6 +177,32 @@ TfLiteStatus CalculateOpData(TfLiteContext* context, TfLiteNode* node) {
     TF_LITE_ENSURE_STATUS(CalculateActivationRangeQuantized(
         context, kTfLiteActNone, output, &data->output_activation_min,
         &data->output_activation_max));
+  }
+  else if (output->type == kTfLiteInt16) {
+    node->user_data =
+        context->AllocatePersistentBuffer(context, sizeof(OpData));
+    OpData* data = static_cast<OpData*>(node->user_data);
+
+    data->input_offset = -input->params.zero_point;
+    data->output_offset = output->params.zero_point;
+    data->left_shift = kCumSumIntegerShift_s16;
+    const double twice_max_input_scale =
+        2 * static_cast<double>(input->params.scale);
+    const double real_input_multiplier =
+        static_cast<double>(input->params.scale) / twice_max_input_scale;
+    const double real_output_multiplier =
+        twice_max_input_scale /
+        ((1 << data->left_shift) * static_cast<double>(output->params.scale));
+    QuantizeMultiplierSmallerThanOneExp(
+        real_input_multiplier, &data->input_multiplier, &data->input_shift);
+
+    QuantizeMultiplierSmallerThanOneExp(
+        real_output_multiplier, &data->output_multiplier, &data->output_shift);
+        
+    TF_LITE_ENSURE_STATUS(CalculateActivationRangeQuantized(
+        context, kTfLiteActNone, output, &data->output_activation_min,
+        &data->output_activation_max));
+
   }
 
   micro_context->DeallocateTempTfLiteTensor(input);
@@ -156,8 +264,27 @@ TfLiteStatus CumSumEval(TfLiteContext* context, TfLiteNode* node) {
       return kTfLiteOk;
     } break;
 
+    case kTfLiteInt16: {
+      auto* data = static_cast<OpData*>(node->user_data);
+      ArithmeticParams params;
+      params.left_shift = data->left_shift;
+      params.input1_offset = data->input_offset;
+      params.input1_multiplier = data->input_multiplier;
+      params.input1_shift = data->input_shift;
+      params.output_offset = data->output_offset;
+      params.output_multiplier = data->output_multiplier;
+      params.output_shift = data->output_shift;
+      SetActivationParams(data->output_activation_min,
+                          data->output_activation_max, &params);
+      CumSum_s16(params, tflite::micro::GetTensorData<int16_t>(input),
+                            input_shape, axis, cs_params->exclusive,
+                            cs_params->reverse,
+                            tflite::micro::GetTensorData<int16_t>(output));
+      return kTfLiteOk;
+    }
+
     default: {
-      MicroPrintf("CUMSUM only supports FLOAT32 and INT8, got %s.",
+      MicroPrintf("CUMSUM only supports FLOAT32, INT8, and INT16, got %s.",
                   TfLiteTypeGetName(output->type));
       return kTfLiteError;
     }
