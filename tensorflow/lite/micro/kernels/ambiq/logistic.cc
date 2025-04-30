03bc6b10ea1071447286303e10870dd68dba5322
John Connolly <john.will.connolly@gmail.com>
2025-04-30 10:17:12 -0400

    remove nnactivations header reference

--- tensorflow/lite/micro/kernels/ambiq/logistic.cc
+++ tensorflow/lite/micro/kernels/ambiq/logistic.cc
@@ -16,7 +16,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/reference/integer_ops/logistic.h"
 
 #include "Include/arm_nnsupportfunctions.h"
-#include "Include/arm_nnactivations.h"
+#include "Include/arm_nnfunctions.h"
 
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
