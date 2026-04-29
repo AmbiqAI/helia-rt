ae15b901bf2c4f286797a90e4e7736b111d960fe
Evan <bugena123@gmail.com>
2026-04-28 17:45:25 -0700

    feat: Add ap3 and ap4 support to memory_regions.h

--- include/memory_regions.h
+++ include/memory_regions.h
@@ -1,13 +1,20 @@
-#ifndef HELIA_RT_ZEPHYR_MEMORY_REGIONS_H_
-#define HELIA_RT_ZEPHYR_MEMORY_REGIONS_H_
+#ifndef HELIA_RT_MEMORY_REGIONS_H_
+#define HELIA_RT_MEMORY_REGIONS_H_
 
 #include <zephyr/linker/section_tags.h>
 #include <zephyr/toolchain.h>
 
 /*
- * This header is currently intended only for Zephyr applications that consume
- * helia-rt, either from source modules or from the prebuilt release bundle.
+ * Apollo3x and Apollo4x expose a single TCM region through Zephyr's ITCM
+ * linker section. Apollo5x provides a dedicated DTCM linker region.
  */
+#if defined(CONFIG_SOC_SERIES_APOLLO3X) || defined(CONFIG_SOC_SERIES_APOLLO4X)
+#define HELIA_RT_TCM_DATA_SECTION __itcm_section
+#define HELIA_RT_TCM_BSS_SECTION __itcm_section
+#else
+#define HELIA_RT_TCM_DATA_SECTION __dtcm_data_section
+#define HELIA_RT_TCM_BSS_SECTION __dtcm_bss_section
+#endif
 
 /*
  * Model/flatbuffer placement options.
@@ -18,7 +25,7 @@
  */
 #define HELIA_RT_MODEL_REGION_MRAM const
 #define HELIA_RT_MODEL_REGION_SRAM Z_GENERIC_SECTION(.data)
-#define HELIA_RT_MODEL_REGION_DTCM __dtcm_data_section
+#define HELIA_RT_MODEL_REGION_DTCM HELIA_RT_TCM_DATA_SECTION
 
 /*
  * Writable tensor arena placement options.
@@ -28,6 +35,6 @@
  *   alignas(16) HELIA_RT_ARENA_REGION_DTCM uint8_t tensor_arena[kTensorArenaSize];
  */
 #define HELIA_RT_ARENA_REGION_SRAM
-#define HELIA_RT_ARENA_REGION_DTCM __dtcm_bss_section
+#define HELIA_RT_ARENA_REGION_DTCM HELIA_RT_TCM_BSS_SECTION
 
-#endif /* HELIA_RT_ZEPHYR_MEMORY_REGIONS_H_ */
+#endif /* HELIA_RT_MEMORY_REGIONS_H_ */
