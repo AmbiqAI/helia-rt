36c5e0f3507b00cc994141885e15e7ee5dc03192
Adam Page <apage224@gmail.com>
2026-09-22 17:50:17 -0500

    style(helia): wrap an 81-column line
    Refs #238

--- tensorflow/lite/micro/kernels/helia/fully_connected.cc
+++ tensorflow/lite/micro/kernels/helia/fully_connected.cc
@@ -193,8 +193,9 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
           data->kernel_sums, filter_dims.n, data->output_depth, filter_data,
           input_offset, filter_offset, tflite::GetTensorData<int32_t>(bias));
       if (vector_sum_status != ARM_CMSIS_NN_SUCCESS) {
-        MicroPrintf("FULLY_CONNECTED: arm_vector_sum_s8 failed at Prepare (%d).",
-                    static_cast<int>(vector_sum_status));
+        MicroPrintf(
+            "FULLY_CONNECTED: arm_vector_sum_s8 failed at Prepare (%d).",
+            static_cast<int>(vector_sum_status));
         return kTfLiteError;
       }
 
